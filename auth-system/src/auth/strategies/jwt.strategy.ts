c9058c438aad3ddc248581068dc53140ca6143da
ZidaneDanz <96367567+ZidaneDanz@users.noreply.github.com>
2025-03-28 12:23:03 +0700

    Add auth-system/src/auth/strategies/jwt.strategy.ts 1743139383140

---
+++ auth-system/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,36 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from '../../user/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    @InjectModel(User)
+    private readonly userModel: typeof User,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.userModel.findByPk(payload.sub);
+    
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    };
+  }
+}
